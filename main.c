f7fd0961713f73ebf71f62d4c3b3a5fc13fa426c
spoturno <spoturno16@gmail.com>
2021-07-14 22:59:55 -0300

    Execution time in main added

--- main.c
+++ main.c
@@ -6,9 +6,13 @@
 
 int main(void){
     //test with isPrime_1
+    clock_t begin = clock();
     nat n;
     printf("Insert Number:");
     scanf("%u", &n);
     (isPrime_1(n) ? printf("%u is prime\n", n) : printf("%u is NOT prime\n", n));
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("exec-time: %f sec\n", time_spent);
     return 0;
 }
