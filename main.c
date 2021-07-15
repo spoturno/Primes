c17593d5df4e7e7021e062826f36ecd26600d0d0
spoturno <spoturno16@gmail.com>
2021-07-14 23:24:41 -0300

    testing big number

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
+#include <math.h>
 
 #include "include/prime_t.h"
 
@@ -14,5 +15,7 @@ int main(void){
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("exec-time: %f sec\n", time_spent);
+    unsigned long long int b = 6549765498764654;
+    printf("%llu", b);
     return 0;
 }
