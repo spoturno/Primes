0787f04809c74a52e75d78396ec3bd94c12d00cf
spoturno <spoturno16@gmail.com>
2021-07-24 00:43:39 -0300

    Update prime_test.h

--- include/prime_test.h
+++ include/prime_test.h
@@ -5,10 +5,20 @@
 
 typedef unsigned int nat; //natural number
 
+/*
+ * Checks if the given numbers n is prime using sieve of eratosthenes 
+*/
 bool isPrime_1(nat n);
 
+/*
+ * checks if the given number n is prime by comparing with big prime list
+ * (src/primes.txt) reading the filestream and storing each value in an array
+*/
 bool isPrime_2(int n);
 
+/*
+ * This will change (TODO) 
+*/
 bool isPrime_3(int n);
 
 #endif
